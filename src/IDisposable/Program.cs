57194af4fda9b531ff9f1ceb437b2c785b5caeb0
Danilo O. Pinheiro <64677271+daniloopinheiro@users.noreply.github.com>
2023-06-22 00:26:56 +0000

    first commit

---
+++ src/IDisposable/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace IDisposable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Utilizando a interface IDisposable");
+            Console.WriteLine("==================================");
+            
+            Console.WriteLine("Método não indicado");
+            Console.WriteLine("----------------------------------");
+            // ClasseRuim().ClasseRuim("");
+
+            Console.WriteLine("Método indicado");
+            Console.WriteLine("----------------------------------");
+            // ClasseBom().ClasseBom();
+        }
+    }
+
+    // public class ClasseRuim
+    // {
+    //     public string File { get; private set; }
+    //     private System.IO.Stream _stream;    
+    //     // public ClasseRuim(string filePath) => _stream = File.OpenRead(filePath);
+    //     public ClasseRuim (string filePath)
+    //     {
+    //          _stream = File.OpenRead(filePath); 
+    //     }
+    // }
+
+    //  public class ClasseBom : IDisposable
+    // {
+    //     public string File { get; private set; }
+    //     private System.IO.Stream _stream;    
+    //     // public ClasseRuim(string filePath) => _stream = File.OpenRead(filePath);
+    //     public ClasseBom (string filePath)
+    //     {
+    //          _stream = File.OpenRead(filePath); 
+    //     }
+    // }
+}
